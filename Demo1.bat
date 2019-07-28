e481beeac7d5f82a4edde56027b595744ce07ddb
Sudhakar <sudhakar0210@gmail.com>
2019-07-28 22:42:11 +0530

    First Comments

---
+++ Demo1.bat
@@ -0,0 +1,9 @@
+set projectLocation=D:\Selenium\Selenium_Scripts\TestDemo
+ 
+cd %projectLocation%
+ 
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+ 
+java org.testng.TestNG %projectLocation%\testng.xml
+ 
+pause
---
+++ src/Demo1.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+   @Test
+   public void openGoogleApp()
+   {
+	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\mypc\\Desktop\\Selenium Softwares\\chromedriver.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		System.out.println(driver.getTitle());
+   }
+}
